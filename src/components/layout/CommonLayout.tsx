ba57f99cd97e3b7faeaebec979c8bfb65375df0e
rafizul896 <rafizulislam896@gmail.com>
2026-07-26 18:59:10 +0600

    updated

--- src/components/layout/CommonLayout.tsx
+++ src/components/layout/CommonLayout.tsx
@@ -1,6 +1,6 @@
-import type { ReactNode } from "react";
-import Navbar from "../shared/Nabvar";
-import Footer from "../shared/Footer";
+import { ReactNode } from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 interface IProps {
   children: ReactNode;
@@ -8,9 +8,9 @@ interface IProps {
 
 export default function CommonLayout({ children }: IProps) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className=" min-h-screen flex flex-col">
       <Navbar />
-      <div className="grow">{children}</div>
+      <div className="grow-1">{children}</div>
       <Footer />
     </div>
   );
